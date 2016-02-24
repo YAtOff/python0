5eb4c80f59c359a456d7745974d37cc78552af2a
Yavor Atov <yavor.atov@gmail.com>
2016-02-24 23:22:57 +0200

    Added tasks for week 17

---
+++ week17/string_functions.py
@@ -0,0 +1,34 @@
+def volwels_count(s):
+    """
+    >>> volwels_count('hello')
+    2
+    >>> volwels_count('I\'m fine')
+    3
+    """
+    pass
+
+
+def cons_count(s):
+    """
+    >>> cons_count('hello')
+    3
+    >>> cons_count('I\'m fine')
+    3
+    """
+    pass
+
+
+def snake_to_camel(s):
+    """
+    >>> snake_to_camel('snake_to_camel')
+    snakeToCamel
+    """
+    pass
+
+
+def snake_to_camel(s):
+    """
+    >>> snake_to_pascal('snake_to_pascal')
+    SnakeToPascal
+    """
+    pass

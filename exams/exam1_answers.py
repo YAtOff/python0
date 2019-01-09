49bb0cdfda6706672fa5d407bab5b6314df6980f
Yavor Atov <yavor.atov@gmail.com>
2019-01-09 23:00:33 +0200

    Add exam 1

---
+++ exams/exam1_answers.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+print('*' * 80)
+print('Вариант 1')
+
+print('-' * 80)
+print('1. Какъва е стойността на следните изрази:')
+print('2 ** 3 / 4', 2 ** 3 / 4)
+print('(5 // 3) ** 1.5', (5 // 3) ** 1.5)
+print('5 % 2 - 1', 5 % 2 - 1)
+
+print('-' * 80)
+print('2. Какъва е стойността на следните изрази:')
+print('5 > 4 and 2 > 1 or 3 < 5', 5 > 4 and 2 > 1 or 3 < 5)
+print('True and False or not False', True and False or not False)
+print('2 ** 5 > 29 and 5 < 20 // 3', 2 ** 5 > 29 and 5 < 20 // 3)
+
+print('-' * 80)
+print('4. Какво ще се изпечата?')
+
+temp = 100
+temp = temp - 100
+if temp > 99:
+    print("Hot")
+elif temp > 100:
+    print("REALLY HOT!")
+elif temp > 60:
+    print("Comfortable")
+else:
+    print("Cold")
+
+print('*' * 80)
+print('Вариант 2')
+
+print('-' * 80)
+print('1. Какъва е стойността на следните изрази:')
+print('5 ** 2 / 4', 5 ** 2 / 4)
+print('(10 // 6) ** 9.9', (10 // 6) ** 9.9)
+print('11 % 2 - 7', 11 % 2 - 7)
+
+print('-' * 80)
+print('2. Какъва е стойността на следните изрази:')
+print('7 > 3 and 10 > 11 or 5 < 10', 7 > 3 and 10 > 11 or 5 < 10)
+print('False or True and not True', False or True and not True)
+print('10 ** 2 > 111 or 3 < 8 / 3 and 5 > 30 % 20', 10 ** 2 > 111 or 3 < 8 / 3 and 5 > 30 % 20)
+
+print('-' * 80)
+print('4. Какво ще се изпечата?')
+
+temp = 10
+temp = temp ** temp
+if temp > 99:
+    print("Hot")
+elif temp > 100:
+    print("REALLY HOT!")
+elif temp > 60:
+    print("Comfortable")
+else:
+    print("Cold")
+
+print('*' * 80)
+print('Вариант 3')
+
+print('-' * 80)
+print('1. Какъва е стойността на следните изрази:')
+print('4 ** 4 / 4', 4 ** 4 / 4)
+print('(11 // 10) ** 8.1', (11 // 10) ** 8.1)
+print('13 % 3 - 9', 13 % 3 - 9)
+
+print('-' * 80)
+print('2. Какъва е стойността на следните изрази:')
+print('10 > 9 and 5 > 7 or 3 < 11', 10 > 9 and 5 > 7 or 3 < 11)
+print('True and (False or True) and not False', True and (False or True) and not False)
+print('4 ** 2 > 60 or 3 < 10 // 3 and 5 > 10 % 2', 4 ** 2 > 60 or 3 < 10 // 3 and 5 > 10 % 2)
+
+print('-' * 80)
+print('4. Какво ще се изпечата?')
+
+temp = 8
+temp = temp * temp
+if temp > 99:
+    print("Hot")
+elif temp > 100:
+    print("REALLY HOT!")
+elif temp > 60:
+    print("Comfortable")
+else:
+    print("Cold")
